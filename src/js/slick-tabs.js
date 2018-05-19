c74df7eff438e9658b2a434139bf5195776f2fd4
romeo gogokhia <romeo.gogokhia@gmail.com>
2018-05-19 20:49:49 +0400

    main page markap end. need to fix issues

--- src/js/slick-tabs.js
+++ src/js/slick-tabs.js
@@ -1,27 +1,28 @@
 // slick in tabs
 function Tabs() {
-    let bindAll = function() {
-        let menuElements = document.querySelectorAll('[data-tab]');
-        for(let i = 0; i < menuElements.length ; i++) {
-            menuElements[i].addEventListener('click', change, false);
-        }
-    }
-
     let clear = function() {
         let menuElements = document.querySelectorAll('[data-tab]');
         for(let i = 0; i < menuElements.length ; i++) {
+            console.log(menuElements[i]);
             menuElements[i].classList.remove('active');
             let id = menuElements[i].getAttribute('data-tab');
             document.getElementById(id).classList.remove('active');
         }
-    }
+    };
 
     let change = function(e) {
         clear();
         e.target.classList.add('active');
         let id = e.currentTarget.getAttribute('data-tab');
         document.getElementById(id).classList.add('active');
-    }
+    };
+
+    let bindAll = function() {
+        let menuElements = document.querySelectorAll('[data-tab]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].addEventListener('click', change, false);
+        }
+    };
 
     bindAll();
 }
