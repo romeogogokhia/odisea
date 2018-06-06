c31cd394dbe5c022e9ef796a6b40a28f3fb52a39
romeo gogokhia <romeo.gogokhia@gmail.com>
2018-06-06 17:37:12 +0400

    index.html end

--- src/js/scripts/slick-tabs.js
+++ src/js/scripts/slick-tabs.js
@@ -1,9 +1,9 @@
 // slick in tabs
+
 function Tabs() {
     let clear = function() {
         let menuElements = document.querySelectorAll('[data-tab]');
         for(let i = 0; i < menuElements.length ; i++) {
-            console.log(menuElements[i]);
             menuElements[i].classList.remove('active');
             let id = menuElements[i].getAttribute('data-tab');
             document.getElementById(id).classList.remove('active');
@@ -27,4 +27,92 @@ function Tabs() {
     bindAll();
 }
 
-let connectTabs = new Tabs();
+new Tabs();
+
+
+function HandmadeTabs() {
+    let clear = function() {
+        let menuElements = document.querySelectorAll('[data-tab-handmade]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].classList.remove('active');
+            let id = menuElements[i].getAttribute('data-tab-handmade');
+            document.getElementById(id).classList.remove('active');
+        }
+    };
+
+    let change = function(e) {
+        clear();
+        e.target.classList.add('active');
+        let id = e.currentTarget.getAttribute('data-tab-handmade');
+        document.getElementById(id).classList.add('active');
+    };
+
+    let bindAll = function() {
+        let menuElements = document.querySelectorAll('[data-tab-handmade]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].addEventListener('click', change, false);
+        }
+    };
+
+    bindAll();
+}
+
+new HandmadeTabs()
+
+function CosmeticTabs() {
+    let clear = function() {
+        let menuElements = document.querySelectorAll('[data-tab-cosmetics]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].classList.remove('active');
+            let id = menuElements[i].getAttribute('data-tab-cosmetics');
+            document.getElementById(id).classList.remove('active');
+        }
+    };
+
+    let change = function(e) {
+        clear();
+        e.target.classList.add('active');
+        let id = e.currentTarget.getAttribute('data-tab-cosmetics');
+        document.getElementById(id).classList.add('active');
+    };
+
+    let bindAll = function() {
+        let menuElements = document.querySelectorAll('[data-tab-cosmetics]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].addEventListener('click', change, false);
+        }
+    };
+
+    bindAll();
+}
+
+new CosmeticTabs();
+
+function BookTabs() {
+    let clear = function() {
+        let menuElements = document.querySelectorAll('[data-tab-books]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].classList.remove('active');
+            let id = menuElements[i].getAttribute('data-tab-books');
+            document.getElementById(id).classList.remove('active');
+        }
+    };
+
+    let change = function(e) {
+        clear();
+        e.target.classList.add('active');
+        let id = e.currentTarget.getAttribute('data-tab-books');
+        document.getElementById(id).classList.add('active');
+    };
+
+    let bindAll = function() {
+        let menuElements = document.querySelectorAll('[data-tab-books]');
+        for(let i = 0; i < menuElements.length ; i++) {
+            menuElements[i].addEventListener('click', change, false);
+        }
+    };
+
+    bindAll();
+}
+
+new BookTabs();
