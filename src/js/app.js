e76cf0c43cd86e1ec4c999893cca777751e99f96
romeo gogokhia <romeo.gogokhia@gmail.com>
2018-06-06 22:06:57 +0400

    2. product-list.html end

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,6 @@ window.$ = $;
 
 import "./vendor/vendor"
 import "./scripts/slick-tabs"
-import "./scripts/noUiSlider"
 
 window.slick = require('slick-carousel');
 
@@ -75,4 +74,21 @@ if(window.screen.width < 576) {
 }
 
 
+let productListTitle = document.querySelectorAll('.product-list-title');
 
+productListTitle.forEach(list => {
+    list.addEventListener('click', function (e) {
+        if(this.classList.contains('minus')){
+            this.classList.remove('minus');
+            this.classList.add('plus');
+            this.parentNode.nextElementSibling.style.display = 'none';
+        }else{
+            this.classList.remove('plus');
+            this.classList.add('minus');
+            this.parentNode.nextElementSibling.style.display = 'block';
+        }
+    })
+
+
+    console.log(list.parentNode.nextElementSibling)
+})
--- src/js/scripts/noUiSlider.js
+++ src/js/scripts/noUiSlider.js
@@ -1,21 +1,24 @@
-/*
-let product_list_range = document.getElementById('product_list_range');
-let product_list_price_min = document.getElementById('product_list_price_min');
-let product_list_price_max = document.getElementById('product_list_price_max');
+document.addEventListener("DOMContentLoaded", function() {
 
-noUiSlider.create(product_list_range, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 100
-    }
+    let product_list_range = document.getElementById('product_list_range');
+    let product_list_price_min = document.getElementById('product_list_price_min');
+    let product_list_price_max = document.getElementById('product_list_price_max');
+
+    noUiSlider.create(product_list_range, {
+        start: [20, 80],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
+
+    product_list_range.noUiSlider.on('update', function (values) {
+        product_list_price_min.value = values[0];
+        product_list_price_max.value = values[1];
+    });
+
 });
 
-product_list_range.noUiSlider.on('update', function (values) {
-    product_list_price_min.value = values[0];
-    product_list_price_max.value = values[1];
-});
-*/
 
 
