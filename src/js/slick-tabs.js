f48e58cb9e85dc2de7f301b35cecd1df2e0dd250
romeo gogokhia <romeo.gogokhia@gmail.com>
2018-06-07 13:50:30 +0400

    6. register.html end need social icons

--- src/js/slick-tabs.js
+++ src/js/slick-tabs.js
@@ -1,35 +1,5 @@
 // slick in tabs
 
-function Tabs() {
-    let clear = function() {
-        let menuElements = document.querySelectorAll('[data-tab]');
-        for(let i = 0; i < menuElements.length ; i++) {
-            menuElements[i].classList.remove('active');
-            let id = menuElements[i].getAttribute('data-tab');
-            document.getElementById(id).classList.remove('active');
-        }
-    };
-
-    let change = function(e) {
-        clear();
-        e.target.classList.add('active');
-        let id = e.currentTarget.getAttribute('data-tab');
-        document.getElementById(id).classList.add('active');
-    };
-
-    let bindAll = function() {
-        let menuElements = document.querySelectorAll('[data-tab]');
-        for(let i = 0; i < menuElements.length ; i++) {
-            menuElements[i].addEventListener('click', change, false);
-        }
-    };
-
-    bindAll();
-}
-
-new Tabs();
-
-
 function HandmadeTabs() {
     let clear = function() {
         let menuElements = document.querySelectorAll('[data-tab-handmade]');
